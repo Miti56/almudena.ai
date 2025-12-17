e9c16a8e85f47a32fe0e3f7beb41ad785f15beb4
miti <miguel.monereo@gmail.com>
2025-12-17 10:11:42 +0100

    added safaty against bots

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [
         tailwindcss(),
@@ -12,4 +11,7 @@ export default defineConfig({
             },
         }),
     ],
+    build: {
+        sourcemap: false,   // ⬅️ IMPORTANT
+    },
 })
